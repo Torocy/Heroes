f6c5e545782eb2b718fe9f4521cb694e5dc93ab9
Torocy Ojweke <torocy45@gmail.com>
2018-10-08 12:00:31 +0300

    added a mock heroes file

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,14 +2,14 @@ import {Hero} from './heroes';
 
 export const HEROES : Hero[] = [
 {id:11, name: 'Ojweke'},
-{id:11, name: 'Henry'},
-{id:11, name: 'Warinda'},
-{id:11, name: 'Guyo'},
-{id:11, name: 'Paul'},
-{id:11, name: 'Karani'},
-{id:11, name: 'Odeny'},
-{id:11, name: 'Peggy'},
-{id:11, name: 'Okeyo'},
-{id:11, name: 'John'}
+{id:12, name: 'Henry'},
+{id:13, name: 'Warinda'},
+{id:14, name: 'Guyo'},
+{id:15, name: 'Paul'},
+{id:16, name: 'Karani'},
+{id:17, name: 'Odeny'},
+{id:18, name: 'Peggy'},
+{id:19, name: 'Okeyo'},
+{id:20, name: 'John'}
 
 ];
