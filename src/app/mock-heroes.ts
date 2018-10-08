e4d6d605da79ea0cec608b7fe16b8fcfa7db1afc
Torocy Ojweke <torocy45@gmail.com>
2018-10-08 09:57:47 +0300

    created a mock heroes file

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './heroes';
+
+export const HEROES : Hero[] = [
+{id:11, name: 'Ojweke'},
+{id:11, name: 'Henry'},
+{id:11, name: 'Warinda'},
+{id:11, name: 'Guyo'},
+{id:11, name: 'Paul'},
+{id:11, name: 'Karani'},
+{id:11, name: 'Odeny'},
+{id:11, name: 'Peggy'},
+{id:11, name: 'Okeyo'},
+{id:11, name: 'John'}
+
+];
