03140ac780165230ee3613e73c817e4ec6bf622e
Torocy Ojweke <torocy45@gmail.com>
2018-10-18 15:58:17 +0300

    added messages component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../heroes';
-import { HEROES} from '../mock-heroes';
+import { HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,16 +14,23 @@ export class HeroesComponent implements OnInit {
     name :'Torocy'
   };
 
-  heroes = HEROES;
   selectedHero : Hero;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor( private heroService : HeroService) { }
 
   ngOnInit() {
+    //call the function.
+    this.getHeroes();
   }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  //function to retrieve the heroes from the service.
+  getHeroes(): void{
+    //subscribe in heroes component.
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes); 
   }
 
 }
